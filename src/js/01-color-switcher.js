02c0590e183f61d764957619f82be55031b7db13
kesobyte <kimangeloalvarez@gmail.com>
2024-04-19 17:14:41 +0800

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -11,19 +11,19 @@ const startBtn = document.querySelector('button[data-start]');
 let colorChangeInterval = null;
 stopBtn.disabled = true;
 
-const startColorChange = () => {
+function startColorChange() {
   colorChangeInterval = setInterval(() => {
     body.style.backgroundColor = getRandomHexColor();
     stopBtn.disabled = false;
     startBtn.disabled = true;
   }, 1000);
-};
+}
 
-const stopColorChange = () => {
+function stopColorChange() {
   clearInterval(colorChangeInterval);
   startBtn.disabled = false;
   stopBtn.disabled = true;
-};
+}
 
 //Event Listener
 startBtn.addEventListener('click', startColorChange);
