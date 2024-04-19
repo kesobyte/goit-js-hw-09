e2b312174bfe2f1064567d6f298a2f1a3d0650f2
kesobyte <147930098+kesobyte@users.noreply.github.com>
2024-04-19 09:17:59 +0000

    Deploying to gh-pages from @ kesobyte/goit-js-hw-09@4f67aeb17d28d0af20d99111ae53d99a0ca19593 🚀

--- 01-color-switcher.b270a961.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("body"),e=document.querySelector("button[data-stop]"),d=document.querySelector("button[data-start]");let a=null;e.disabled=!0,d.addEventListener("click",(function(){a=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`,e.disabled=!1,d.disabled=!0}),1e3)})),e.addEventListener("click",(function(){clearInterval(a),d.disabled=!1,e.disabled=!0}));
-//# sourceMappingURL=01-color-switcher.b270a961.js.map
