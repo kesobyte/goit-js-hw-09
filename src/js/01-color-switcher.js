1a6519b8673fda9fcbdacbbb30ae9f2a9d85ed3b
kesobyte <kimangeloalvarez@gmail.com>
2024-04-19 17:08:36 +0800

    Updates

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const body = document.querySelector('body');
+const stopBtn = document.querySelector('button[data-stop]');
+const startBtn = document.querySelector('button[data-start]');
+
+let colorChangeInterval = null;
+stopBtn.disabled = true;
+
+const startColorChange = () => {
+  colorChangeInterval = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+    stopBtn.disabled = false;
+    startBtn.disabled = true;
+  }, 1000);
+};
+
+const stopColorChange = () => {
+  clearInterval(colorChangeInterval);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+};
+
+//Event Listener
+startBtn.addEventListener('click', startColorChange);
+stopBtn.addEventListener('click', stopColorChange);
